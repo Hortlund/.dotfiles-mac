a8e2ad2c0db6cb00e93c7ef2f2b3fb12344f3c26
Andreas Hortlund <andreas.hortlund@askas.se>
2022-09-12 08:46:28 +0200

    Devicons

--- .config/nvim/lua/hortlund/packer.lua
+++ .config/nvim/lua/hortlund/packer.lua
@@ -19,7 +19,7 @@ return require('packer').startup(function()
 	}
 	use {
   		'nvim-lualine/lualine.nvim',
-  		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  		requires = { 'kyazdani42/nvim-web-devicons'}
 	}
 	use {
   		'pwntester/octo.nvim',
--- .config/nvim/plugin/packer_compiled.lua
+++ .config/nvim/plugin/packer_compiled.lua
@@ -135,9 +135,8 @@ _G.packer_plugins = {
     url = "https://github.com/nvim-treesitter/nvim-treesitter-context"
   },
   ["nvim-web-devicons"] = {
-    loaded = false,
-    needs_bufread = false,
-    path = "/Users/andreas.hortlund/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
+    loaded = true,
+    path = "/Users/andreas.hortlund/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["octo.nvim"] = {
