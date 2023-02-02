c800ee8af4f3b71625b080495dbd5f65fa4ba6c2
Andreas Hortlund <andreas.hortlund@askas.se>
2023-02-02 08:55:10 +0100

    Snippets

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -13,12 +13,30 @@ lsp.ensure_installed({
 })
 
 local cmp = require('cmp')
+local luasnip = require('luasnip')
 local cmp_select = {behavior = cmp.SelectBehavior.Select}
 local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
   ["<C-Space>"] = cmp.mapping.complete(),
+    -- go to next placeholder in the snippet
+  ['<Tab>'] = cmp.mapping(function(fallback)
+    if luasnip.jumpable(1) then
+      luasnip.jump(1)
+    else
+      fallback()
+    end
+  end, {'i', 's'}),
+  
+  -- go to previous placeholder in the snippet
+  ['<S-Tab>'] = cmp.mapping(function(fallback)
+    if luasnip.jumpable(-1) then
+      luasnip.jump(-1)
+    else
+      fallback()
+    end
+  end, {'i', 's'}),
 })
 
 lsp.set_preferences({
